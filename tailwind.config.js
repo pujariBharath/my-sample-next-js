2e02e1c9154797a1ad69b2dc2ed7497628b9814a
Bharath <132049800+pujariBharath@users.noreply.github.com>
2024-11-27 10:47:42 +0530

    v.utcgj

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    content: ["./app/**/*.{js,jsx,ts,tsx}", "./app/globals.css"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,5 @@
 import type { Config } from "tailwindcss";
-module.exports = {
-	content: [
-	  "./app/**/*.{js,ts,jsx,tsx}",
-	  "./pages/**/*.{js,ts,jsx,tsx}",
-	  "./components/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-	  extend: {},
-	},
-	plugins: [],
-  };
-  
-  
-
-module.exports = {
-	content: [
-	  "./pages/**/*.{js,ts,jsx,tsx}",
-	  "./components/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-	  extend: {},
-	},
-	plugins: [],
-  };
-  
+ 
 
 export default {
     darkMode: ["class"],
