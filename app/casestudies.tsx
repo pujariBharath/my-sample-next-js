ce41d776d7c76d8dea15c2cdfe19eb68952096b7
Your Name <you@example.com>
2024-11-28 14:54:18 +0530

    cd

--- app/casestudies.tsx
+++ app/casestudies.tsx
@@ -1,37 +1,7 @@
 import React from "react";
 
-const CaseStudies = () => {
-  const caseStudies = [
-    {
-      id: 1,
-      title: "Case Study 1",
-      description: "Description of case study 1.",
-    },
-    {
-      id: 2,
-      title: "Case Study 2",
-      description: "Description of case study 2.",
-    },
-    {
-      id: 3,
-      title: "Case Study 3",
-      description: "Description of case study 3.",
-    },
-  ];
-
-  return (
-    <div style={{ padding: "20px" }}>
-      <h1>Case Studies</h1>
-      <ul>
-        {caseStudies.map((study) => (
-          <li key={study.id} style={{ marginBottom: "20px" }}>
-            <h2>{study.title}</h2>
-            <p>{study.description}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+const Hello = () => {
+  return <h1>Hello World</h1>;
 };
 
-export default CaseStudies;
+export default Hello;
