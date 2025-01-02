8f39409b97a64df3f517a538aea128611ef750b9
Bharath <pujaribharath77@gmail.com>
2025-01-02 08:56:23 +0530

    DVS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      borderColor: {
+        border: "hsl(var(--border))",
+      },
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+      }
+    }
+  }
 }
 
